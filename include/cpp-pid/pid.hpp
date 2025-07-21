4cdb82b87aff7940f03f3a98e40729771d9da53b
tommyzhng <tommyz2005@gmail.com>
2025-07-20 23:25:36 -0400

    implement pid with derivative on measurement, change folder structure and add license

--- include/cpp-pid/pid.hpp
+++ include/cpp-pid/pid.hpp
@@ -1,3 +1,32 @@
+/**
+ * @file pid.hpp
+ * @brief Parallel form PID controller implementation with integral clamping and output filtering
+ * @author Tommy Zhang
+ * 
+ * Copyright (c) 2025 Tommy Zhang
+ * SPDX-License-Identifier: MIT
+ * 
+ * This file is part of cpp-pid
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #ifndef PID_HPP
 #define PID_HPP
 
@@ -30,10 +59,10 @@ public:
         setIntegClamp(integ_clamp);
     }
 
-    float run(float setpoint, float currentValue) {
-        error_ = setpoint - currentValue;
+    float run(float setpoint, float measurement) {
+        error_ = setpoint - measurement;
 
-        if (Ts_ > 0.0f) derivative_ = alpha_d_ * (error_ - prev_error_) + (1 - alpha_d_) * derivative_;
+        if (Ts_ > 0.0f) derivative_ = alpha_d_ * (error_ - prev_meas_) + (1 - alpha_d_) * derivative_;
         output_ =  kp_ * error_ + ki_Ts_ * integral_ + kd_Ts_ * derivative_;
 
         if (output_ > max_) output_ = max_; 
@@ -47,7 +76,7 @@ public:
         output_ = alpha_lpf * output_ + (1 - alpha_lpf) * old_output_filtered_; // increasing tau makes output slower
 
         // Update old values for next iteration
-        prev_error_ = error_;
+        prev_meas_ = error_;
         old_output_filtered_ = output_;
 
         return output_;
@@ -55,7 +84,7 @@ public:
 
     void reset() {
         integral_ = 0.0;
-        prev_error_ = 0.0;
+        prev_meas_ = 0.0;
     }
     float integral_;
 
@@ -64,7 +93,7 @@ private:
     float max_, min_ = 0.0f;
     float kp_, ki_Ts_, kd_Ts_, integ_clamp_ = 0.0f;
     float error_, derivative_, alpha_d_, alpha_lpf = 0.0f;
-    float prev_error_, old_error_filtered_, old_output_filtered_ = 0.0;
+    float prev_meas_, old_error_filtered_, old_output_filtered_ = 0.0;
     float output_;
 
     void setKp(float kp) { this->kp_ = kp; }
